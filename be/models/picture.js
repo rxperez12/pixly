039cb277bc36efffcbe51d72f70a681d4e576bf5
Jacob <jacobrashid404@gmail.com>
2024-06-11 16:08:17 -0700

    starter Picture class, WIP. addImageToBucket functional with test inputs.

--- be/models/picture.js
+++ be/models/picture.js
@@ -1,13 +1,42 @@
 import {
   S3Client,
   PutObjectCommand,
-  CreateBucketCommand,
-  DeleteObjectCommand,
-  DeleteBucketCommand,
-  paginateListObjectsV2,
   GetObjectCommand,
+  paginateListObjectsV2
 } from "@aws-sdk/client-s3";
 
-class Picture {
+import {
+  BUCKET_NAME,
+  BUCKET_REGION,
+  ACCESS_KEY,
+  SECRET_ACCESS_KEY
+ } from "../config.js";
 
+
+const s3Client = new S3Client({
+  credentials: {
+    accessKeyId: ACCESS_KEY,
+    secretAccessKey: SECRET_ACCESS_KEY,
+  },
+  region: BUCKET_REGION
+});
+
+class Picture {
+  static async addImageToBucket() {
+    console.log("addImageToBucket");
+    await s3Client.send(
+      new PutObjectCommand({
+        Bucket: BUCKET_NAME,
+        Key: "test key",
+        Body: "test body",
+        ContentType: "test type"
+      })
+    );
+  }
+
+  static async getImagesFromBucket() {
+    console.log("getImagesFromBucket");
+  }
 }
+
+Picture.addImageToBucket();
