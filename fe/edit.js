2b50e48bfecd5c87b627057a1832099d78fc72c3
Jacob <jacobrashid404@gmail.com>
2024-06-12 11:53:31 -0700

    image is loaded onto canvas on page load

--- fe/edit.js
+++ fe/edit.js
@@ -1,17 +1,32 @@
 /** FUNCTIONS FOR EDIT HTML */
 
+// query selectors
+const $canvas = document.querySelector(".canvas");
+const context = $canvas.getContext('2d');
+
+const $sourceImage = document.querySelector(".source-image");
+const $bwButton = document.querySelector(".black-white");
+
+function applyFilters() {
+  //apply filters
+
+  context.drawImage($sourceImage, 0, 0);
+}
 
 
-async function getEditPicture() {
-  const urlParams = new URLSearchParams(window.location.search);
-  const greetingValue = urlParams.get('image');
-  // MAKE API CALL FOR URL
-  //GET URL FROM LOCAL STORAGE???
+function loadImage() {
+  const imgURL = localStorage.getItem("imageURL");
+  $sourceImage.src = imgURL;
+  $sourceImage.onload = function () {
+    $canvas.width = this.width;
+    $canvas.height = this.height;
+    applyFilters();
+  }
 }
 
 
 function start() {
-
+  loadImage();
 }
 
 start();
