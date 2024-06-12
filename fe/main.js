6ede356af60b327f9d91b89945b1e269b2021885
Jacob <jacobrashid404@gmail.com>
2024-06-11 18:13:16 -0700

    getAndShowImages, putImagesOnPage functions added, run on start

--- fe/main.js
+++ fe/main.js
@@ -1,7 +1,27 @@
+const API_URL = "http://localhost:3000/"
+// const $imageArea = document.querySelector(".image-area");
+const $imageList = document.querySelector(".image-list");
 
+async function getAndShowImages() {
+  console.log("getAndShowImages");
+  const response = await fetch(API_URL);
+  const imagesResult = await response.json();
+  putImagesOnPage(imagesResult.images);
+}
+
+function putImagesOnPage(imageURLs) {
+  console.log("putImagesOnPage", imageURLs);
+  for (const url of imageURLs) {
+    const $img = document.createElement("img");
+    $img.src = url;
+    const $li = document.createElement("li");
+    $li.appendChild($img);
+    $imageList.appendChild($li);
+  }
+}
 
 function start() {
-
+  getAndShowImages();
 }
 
 start();
