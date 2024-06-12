96374f6565723726619bb9896e7e93c39a4f00e8
Ricardo Perez <rxperez12@gmail.com>
2024-06-12 12:36:00 -0700

    added editing options for black and white filter

--- fe/edit.js
+++ fe/edit.js
@@ -6,6 +6,7 @@ const context = $canvas.getContext('2d');
 
 const $sourceImage = document.querySelector(".source-image");
 const $bwButton = document.querySelector(".black-white");
+const $presetFilters = document.querySelector(".preset-filters");
 
 function applyFilters() {
   //apply filters
@@ -20,9 +21,52 @@ function loadImage() {
   $sourceImage.onload = function () {
     $canvas.width = this.width;
     $canvas.height = this.height;
-    applyFilters();
-  }
+    context.drawImage(this, 0, 0);
+  };
 }
+
+function resetImage() {
+  const greyScale = `greyscale(0%)`;
+  const brightnessScale = `brightness(100%)`;
+  const contrast = `contrast(100%)`;
+
+}
+
+/** Applies settings for black and white filter and then applies them to canvas */
+function blackWhiteFilter() {
+  console.log('blackwhite filter');
+  const greyScale = `100`;
+  const brightnessScale = `120`;
+  const contrastScale = `120`;
+  applyFilter({ greyScale, brightnessScale, contrastScale });
+}
+
+/** Take argument string settings and applies the settings to the image*/
+function applyFilter(filters) {
+  console.log('Applied filters:', filters);
+
+  let filterString =
+    "brightness(" + filters.brightnessScale + "%" +
+    ") contrast(" + filters.contrastScale + "%" +
+    ") grayscale(" + filters.greyScale + "%" +
+    ") saturate(" + "50" + "%" +
+    ") sepia(" + "0" + "%" +
+    ")";
+
+  context.filter = filterString;
+  context.drawImage($sourceImage, 0, 0);
+}
+
+$presetFilters.addEventListener("click", (evt) => {
+  const $clicked = evt.target;
+
+  if ($clicked.matches('.black-white')) {
+    console.log("clicked on black and white");
+    blackWhiteFilter();
+  }
+
+});
+
 
 
 function start() {
