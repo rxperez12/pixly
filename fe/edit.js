ce194fd3f0866e959eff47e9e1e94aa7c0f8b1ec
Ricardo Perez <rxperez12@gmail.com>
2024-06-12 10:30:38 -0700

    edit intial files

---
+++ fe/edit.js
@@ -0,0 +1,17 @@
+/** FUNCTIONS FOR EDIT HTML */
+
+
+
+async function getEditPicture() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const greetingValue = urlParams.get('image');
+  // MAKE API CALL FOR URL
+  //GET URL FROM LOCAL STORAGE???
+}
+
+
+function start() {
+
+}
+
+start();
