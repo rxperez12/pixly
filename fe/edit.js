ee22251380a0e1ab09e762ef85135bfd3d2ce06d
Jacob <jacobrashid404@gmail.com>
2024-06-12 14:42:01 -0700

    saveImage WIP

--- fe/edit.js
+++ fe/edit.js
@@ -1,4 +1,5 @@
 /** FUNCTIONS FOR EDIT HTML */
+const API_URL = "http://localhost:3000";
 
 // query selectors
 const $canvas = document.querySelector(".canvas");
@@ -7,12 +8,7 @@ const context = $canvas.getContext('2d');
 const $sourceImage = document.querySelector(".source-image");
 const $bwButton = document.querySelector(".black-white");
 const $presetFilters = document.querySelector(".preset-filters");
-
-function applyFilters() {
-  //apply filters
-
-  context.drawImage($sourceImage, 0, 0);
-}
+const $saveButton = document.querySelector(".save-image");
 
 
 function loadImage() {
@@ -21,9 +17,41 @@ function loadImage() {
   $sourceImage.onload = function () {
     $canvas.width = this.width;
     $canvas.height = this.height;
+    $canvas.crossOrigin = "anonymous";
     context.drawImage(this, 0, 0);
   };
 }
+
+async function saveImage() {
+  const canvasData = $canvas.toDataURL("image/png");
+  const blobData = dataURItoBlob(canvasData);
+  console.log("blobData", blobData);
+  const params = {
+    Key: localStorage.getItem("imageKey"),
+    ContentType: "image/png",
+    Body: blobData
+  };
+  const response = await fetch(`${API_URL}/update`, {
+    method: "PUT",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(params)
+  });
+  const result = await response.json();
+  console.log(result);
+}
+$saveButton.addEventListener("click", saveImage);
+
+function dataURItoBlob(dataURI) {
+  const binary = atob(dataURI.split(',')[1]);
+  const array = [];
+  for(let i = 0; i < binary.length; i++) {
+      array.push(binary.charCodeAt(i));
+  }
+  return new Blob([new Uint8Array(array)], {type: 'image/png'});
+}
+
 
 function resetImage() {
   const greyScale = `greyscale(0%)`;
