23c5cce0b3c7be0c495b471c591eb577ec7f67eb
Ricardo Perez <rxperez12@gmail.com>
2024-07-24 21:12:19 -0700

    remove example test

--- be/app.test.js
+++ be/app.test.js
@@ -3,7 +3,4 @@ import request from "supertest";
 
 import app from "./app.js";
 
-test("/", async function() {
-  const resp = await request(app).get("/");
-  expect(resp.text).toEqual("2 + 3 = 5");
-})
+
